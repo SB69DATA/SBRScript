378912d41bc765e393b2b919dd2d51ecf941261f
SB69DATA <sbr.information@gmail.com>
2016-06-19 11:30:17 +0900

    ロングマーカーの強調表示の値調整

--- js/SBRSViewer.js
+++ js/SBRSViewer.js
@@ -875,7 +875,7 @@ var SBRSViewer = (function() {
             break;
           case 3:
             // ロング終了
-            if (SBRSViewer.option.longMarkerFastTap && marker.point % 1 > 0 && marker.point % 1 <= 0.125) {
+            if (SBRSViewer.option.longMarkerFastTap && marker.point % 1 > 0 && marker.point % 1 <= 0.25) {
               markerDiv.className = "long-marker emphasis";
             } else {
               markerDiv.className = "long-marker";
