25ef4ef016acea28e3ea201261f6afd6d01d1e4c
SB69DATA <sbr.information@gmail.com>
2016-07-10 09:36:44 +0900

    WebAudioてす

--- js/SBRSPlayer.js
+++ js/SBRSPlayer.js
@@ -4,8 +4,6 @@ var SBRSPlayer = (function() {
   var sbrs = null;
   var canvas = null;
   var ctx = null;
-  var bgCtx = null;
-  var laneCtx = null;
 
   var CANVAS_WIDTH = 2048 / 2; // CANVASの横幅
   var CANVAS_HEIGHT = 2732 / 2; // CANVASの縦幅
@@ -13,10 +11,14 @@ var SBRSPlayer = (function() {
 
   var JUDGE_Y = 1104; // 判定位置のY座標
   var JUDGE_CIRCLE_SIDE_X = 272; // 判定サークルの画面端からの距離
+  //var JUDGE_CIRCLE_SIZE = 172; // 判定サークルのサイズ
 
   var LANE_TOP_Y = 406; // レーンの上部のY座標
   var LANE_BOTTOM_SIDE_X = 72; // レーンの下部の画面端からの距離
   var LANE_TOP_SIDE_X = 478; // レーンの上部の画面端からの距離
+  var LANE_WIDTH_RATIO = 2.4; // レーンの間の間隔に対するレーン幅の比率
+
+  var SPEAKER_Y = 386; // スピーカーのY座標
 
   var MAX_SE_PLAY = 10; // SEの最大同時再生数
 
@@ -27,6 +29,7 @@ var SBRSPlayer = (function() {
 
   var load = {};
   load.endFlag = false;
+  SBRSPlayer.load = load; // 読み込み関連データ
 
   var img = {};
   img.count = 0;
@@ -40,6 +43,8 @@ var SBRSPlayer = (function() {
   img.longLine = null;
   img.judgeCircle = null;
   img.lane = null;
+  img.insideSpeaker = null;
+  img.outsideSpeaker = null;
   load.img = img;
 
   var sound = {};
@@ -52,7 +57,24 @@ var SBRSPlayer = (function() {
   sound.long = null;
   load.sound = sound;
 
-  SBRSPlayer.load = load; // 読み込みデータ
+  // 汎用
+  var i, iLen;
+
+  // レーン関連
+  var laneCount;
+  var laneTopAllWidth;
+  var laneTopWidth;
+  var laneTopBlankWidth;
+  var laneTopIntervalWidth;
+  var laneBottomAllWidth;
+  var laneBottomWidth;
+  var laneBottomBlankWidth;
+  var laneBottomIntervalWidth;
+
+  // 判定サークル関連
+  var judgeCircleSize;
+  var judgeCircleSideX;
+  var judgeCircleIntervalWidth;
 
   /* function getTime
    * 現在時刻をミリ秒で取得します
@@ -80,16 +102,10 @@ var SBRSPlayer = (function() {
       // キャンバスとコンテキスト取得
       canvas = document.getElementById("main");
       ctx = canvas.getContext("2d");
-      bgCtx = document.getElementById("bg").getContext("2d");
-      laneCtx = document.getElementById("lane").getContext("2d");
 
       // キャンバスのサイズ設定
       canvas.width = CANVAS_WIDTH;
       canvas.height = CANVAS_HEIGHT;
-      document.getElementById("bg").width = CANVAS_WIDTH;
-      document.getElementById("bg").height = CANVAS_HEIGHT;
-      document.getElementById("lane").width = CANVAS_WIDTH;
-      document.getElementById("lane").height = CANVAS_HEIGHT;
 
       // パラメータから譜面のパス取得
       sbrsPath = location.search.match(/load=([^&]*)(&|$)/)[1];
@@ -163,6 +179,8 @@ var SBRSPlayer = (function() {
     img.longLine = null;
     img.judgeCircle = loadImg("img/judge-circle.png");
     img.lane = loadImg("img/lane.png");
+    img.insideSpeaker = loadImg("img/inside-speaker.png");
+    img.outsideSpeaker = loadImg("img/outside-speaker.png");
 
     // 効果音読み込み
     sound.perfect[0] = loadSound("se/perfect.ogg");
@@ -248,11 +266,41 @@ var SBRSPlayer = (function() {
     return Sound;
   }());
 
+  /* function checkLoadEnd
+   * 読み込みの完了確認を行います
+   * 戻り値 : なし
+   */
   function checkLoadEnd() {
-    if (load.img.count === load.img.loadCount /*&&
-      load.sound.count === load.sound.loadCount*/) {
-      load.endFlag = true;
+    if (load.img.count === load.img.loadCount
+      /*&&
+           load.sound.count === load.sound.loadCount*/
+    ) {
+      initPlay();
     }
+  }
+
+  /* function initPlay
+   * 演奏開始前の初期化処理を行います
+   * 戻り値 : なし
+   */
+  function initPlay() {
+    load.endFlag = true;
+
+    // レーン関連
+    laneTopAllWidth = CANVAS_WIDTH - LANE_TOP_SIDE_X * 2;
+    laneBottomAllWidth = CANVAS_WIDTH - LANE_BOTTOM_SIDE_X * 2;
+    laneCount = sbrs.laneCount;
+    laneTopWidth = laneTopAllWidth / (LANE_WIDTH_RATIO * laneCount + laneCount - 1) * LANE_WIDTH_RATIO;
+    laneTopBlankWidth = laneTopAllWidth / (LANE_WIDTH_RATIO * laneCount + laneCount - 1);
+    laneTopIntervalWidth = laneTopWidth + laneTopBlankWidth;
+    laneBottomWidth = laneBottomAllWidth / (LANE_WIDTH_RATIO * laneCount + laneCount - 1) * LANE_WIDTH_RATIO;
+    laneBottomBlankWidth = laneBottomAllWidth / (LANE_WIDTH_RATIO * laneCount + laneCount - 1);
+    laneBottomIntervalWidth = laneBottomWidth + laneBottomBlankWidth;
+
+    // 判定サークル関連
+    judgeCircleSize = laneTopWidth + (laneBottomWidth - laneTopWidth) * ((JUDGE_Y - LANE_TOP_Y) / (CANVAS_HEIGHT - LANE_TOP_Y));
+    judgeCircleSideX = LANE_TOP_SIDE_X - (LANE_TOP_SIDE_X - LANE_BOTTOM_SIDE_X) * ((JUDGE_Y - LANE_TOP_Y) / (CANVAS_HEIGHT - LANE_TOP_Y)) + judgeCircleSize / 2;
+    judgeCircleIntervalWidth = (laneTopIntervalWidth + (laneBottomIntervalWidth - laneTopIntervalWidth) * ((JUDGE_Y - LANE_TOP_Y) / (CANVAS_HEIGHT - LANE_TOP_Y)));
   }
 
   /* function draw
@@ -262,11 +310,6 @@ var SBRSPlayer = (function() {
   function draw(time) {
 
     time = time ? time : getTime();
-
-    // キャンバスクリア
-    //ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    laneCtx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     if (load.endFlag) {
       // 全リソースの読み込み完了
@@ -279,6 +322,9 @@ var SBRSPlayer = (function() {
 
       // マーカーを描画
       drawMarker();
+
+      // スピーカーを描画
+      drawSpeaker();
 
       // 判定サークルを描画
       drawJudgeCircle();
@@ -295,15 +341,7 @@ var SBRSPlayer = (function() {
   //var grad;
 
   function drawBg() {
-    bgCtx.drawImage(img.bg, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    /*
-    grad = ctx.createLinearGradient(0, 0, 0, CANVAS_HEIGHT);
-    grad.addColorStop(0, "rgb(0, 0, 0)");
-    grad.addColorStop(1, "rgb(0, 0, 96)");
-    ctx.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillStyle = grad;
-    ctx.fill();
-    */
+    ctx.drawImage(img.bg, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
 
   /* function drawLane
@@ -311,35 +349,18 @@ var SBRSPlayer = (function() {
    * 戻り値 : なし
    */
   function drawLane() {
-
-    var scale = 2.4;
-    var topWidth = CANVAS_WIDTH - LANE_TOP_SIDE_X * 2;
-    var bottomWidth = CANVAS_WIDTH - LANE_BOTTOM_SIDE_X * 2;
-    var laneCount = sbrs.laneCount;
-    var laneTopWidth = topWidth / (scale * laneCount + laneCount - 1) * scale;
-    var laneTopBlankWidth = topWidth / (scale * laneCount + laneCount - 1);
-    var laneBottomWidth = bottomWidth / (scale * laneCount + laneCount - 1) * scale;
-    var laneBottomBlankWidth = bottomWidth / (scale * laneCount + laneCount - 1);
-    var i, iLen;
-    laneCtx.save();
-
     for (i = 0, iLen = laneCount; i < iLen; i++) {
-    laneCtx.save();
-        laneCtx.fillStyle = "#000";
-      laneCtx.beginPath();
-      laneCtx.moveTo(LANE_TOP_SIDE_X + (laneTopWidth + laneTopBlankWidth) * i, LANE_TOP_Y);
-      laneCtx.lineTo(LANE_BOTTOM_SIDE_X + (laneBottomWidth + laneBottomBlankWidth) * i, CANVAS_HEIGHT);
-      laneCtx.lineTo(LANE_BOTTOM_SIDE_X + (laneBottomWidth + laneBottomBlankWidth) * i + laneBottomWidth, CANVAS_HEIGHT);
-      laneCtx.lineTo(LANE_TOP_SIDE_X + (laneTopWidth + laneTopBlankWidth) * i + laneTopWidth, LANE_TOP_Y);
-      laneCtx.closePath();
-    laneCtx.clip();
-    laneCtx.drawImage(img.lane, 0, LANE_TOP_Y, CANVAS_WIDTH, CANVAS_HEIGHT);
-    laneCtx.restore();
-      //laneCtx.fill();
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(LANE_TOP_SIDE_X + laneTopIntervalWidth * i, LANE_TOP_Y);
+      ctx.lineTo(LANE_BOTTOM_SIDE_X + laneBottomIntervalWidth * i, CANVAS_HEIGHT);
+      ctx.lineTo(LANE_BOTTOM_SIDE_X + laneBottomIntervalWidth * i + laneBottomWidth, CANVAS_HEIGHT);
+      ctx.lineTo(LANE_TOP_SIDE_X + laneTopIntervalWidth * i + laneTopWidth, LANE_TOP_Y);
+      ctx.closePath();
+      ctx.clip();
+      ctx.drawImage(img.lane, 0, LANE_TOP_Y, CANVAS_WIDTH, CANVAS_HEIGHT - LANE_TOP_Y);
+      ctx.restore();
     }
-
-    //laneCtx.globalCompositeOperation = "source-in";
-    laneCtx.restore();
   }
 
   /* function drawMarker
@@ -347,7 +368,16 @@ var SBRSPlayer = (function() {
    * 戻り値 : なし
    */
   function drawMarker() {
-    ctx.drawImageCenter(img.normalMarker, CANVAS_WIDTH / 2, 800, 100, 100);
+    ctx.drawImageCenter(img.normalMarker, CANVAS_WIDTH / 2, 800, 34, 34);
+  }
+
+  /* function drawSpeaker
+   * スピーカーを描画します
+   * 戻り値 : なし
+   */
+  function drawSpeaker() {
+    ctx.drawImageCenter(img.outsideSpeaker, CANVAS_WIDTH / 2, SPEAKER_Y);
+    ctx.drawImageCenter(img.insideSpeaker, CANVAS_WIDTH / 2, SPEAKER_Y);
   }
 
   /* function drawJudgeCircle
@@ -355,10 +385,9 @@ var SBRSPlayer = (function() {
    * 戻り値 : なし
    */
   function drawJudgeCircle() {
-    ctx.drawImageCenter(img.judgeCircle, JUDGE_CIRCLE_SIDE_X, JUDGE_Y, 172, 172);
-    ctx.drawImageCenter(img.judgeCircle, CANVAS_CENTER_X, JUDGE_Y, 172, 172);
-    ctx.drawImageCenter(img.judgeCircle, CANVAS_WIDTH - JUDGE_CIRCLE_SIDE_X, JUDGE_Y, 172, 172);
-
+    for (i = 0, iLen = laneCount; i < iLen; i++) {
+      ctx.drawImageCenter(img.judgeCircle, judgeCircleSideX + judgeCircleIntervalWidth * i, JUDGE_Y, judgeCircleSize, judgeCircleSize);
+    }
   }
 
   /* function drawLoad
@@ -388,3 +417,161 @@ var SBRSPlayer = (function() {
 
   return SBRSPlayer;
 }());
+
+// var WebAudio = (function() {
+
+//   var audioContext = window.AudioContext || window.webkitAudioContext;
+
+//   function WebAudio() {
+//     this.src = "";
+//     this.ctx = null;
+//   }
+
+//   WebAudio.prototype.__defineSetter__("src", function(val) {
+//   });
+
+//   WebAudio.prototype.play = function() {
+
+//   };
+
+//   WebAudio.prototype.pause = function() {
+
+//   };
+
+//   WebAudio.prototype.setVolume = function() {
+
+//   };
+
+//   return WebAudio;
+// }());
+
+window.addEventListener("DOMContentLoaded", function() {
+  var WebAudio = (function() {
+
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioContext = new AudioContext();
+
+    function WebAudio() {
+      this._src = "";
+      this.loop = false;
+      this.readyState = 0;
+      this.loadFlag = false;
+      this.bufferSrc = null;
+      this.buffer = null;
+      this.gainNode = audioContext.createGain();
+      this.event = {};
+    }
+
+    WebAudio.prototype.__defineGetter__("src", function() {
+      return this._src;
+    });
+
+    WebAudio.prototype.__defineSetter__("src", function(val) {
+      if (!val) {
+        this._src = val;
+      }
+      this._src = val;
+
+      var xhr = new XMLHttpRequest();
+      var self = this;
+
+      xhr.open("get", this.src, true);
+      xhr.responseType = "arraybuffer";
+
+      xhr.addEventListener("readystatechange", function() {
+        switch (xhr.readyState) {
+          case 4:
+            if (xhr.status === 200) {
+              audioContext.decodeAudioData(xhr.response, function(buffer) {
+                self.buffer = buffer;
+                self.loadFlag = true;
+                if (typeof self.event.load === "function") {
+                  self.event.load();
+                }
+              });
+            } else {
+
+              if (typeof self.event.error === "function") {
+                self.event.error();
+              }
+
+              throw new Error("読み込みに失敗しました(" + self.src + ")");
+            }
+            break;
+          default:
+            self.readyState = xhr.readyState;
+            break;
+        }
+      });
+
+      xhr.send();
+    });
+
+    WebAudio.prototype.play = function(when, offset) {
+
+      if (!this.loadFlag) {
+        return;
+      }
+
+      when = when ? when : 0;
+      offset = offset ? offset : 0;
+      when = when / 1000.0;
+      offset = offset / 1000.0;
+
+      this.bufferSrc = audioContext.createBufferSource();
+      this.bufferSrc.buffer = this.buffer;
+      this.bufferSrc.connect(this.gainNode);
+      this.bufferSrc.loop = this.loop;
+      this.gainNode.connect(audioContext.destination);
+      if (!this.bufferSrc.loop) {
+        this.bufferSrc.start(when, offset);
+        //this.bufferSrc.start(when + audioContext.currentTime, offset);
+      } else {
+        this.bufferSrc.start(when);
+        //this.bufferSrc.start(when + audioContext.currentTime);
+      }
+    };
+
+    WebAudio.prototype.pause = function(when) {
+
+      if (!this.loadFlag) {
+        return;
+      }
+
+      when = when ? when : 0;
+
+      this.bufferSrc.stop(when + audioContext.currentTime);
+    };
+
+    WebAudio.prototype.setVolume = function(volume) {
+
+      if (!this.loadFlag) {
+        return;
+      }
+
+      volume = volume >= 0 ? volume : 0;
+      volume = volume <= 2 ? volume : 2;
+
+      this.gainNode.gain.value = Math.pow(100, volume) / 100;
+
+    };
+
+    WebAudio.prototype.addEventListener = function(event, callback) {
+      this.event[event] = callback;
+    };
+
+    return WebAudio;
+  })();
+  b = new WebAudio();
+  b.src = "sbrs/CalamityFortuned.ogg";
+  b.addEventListener("load", function() {
+    alert("load")
+  });
+  b.addEventListener("error", function() {
+    alert("error")
+
+  });
+
+  c = new WebAudio();
+  c.src = "se/long.wav";
+});
