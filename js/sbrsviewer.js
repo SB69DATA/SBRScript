df7be31d2b50a1af9569cf19cf362176f45bcb0a
SB69DATA <sbr.information@gmail.com>
2016-09-24 00:41:03 +0900

    ver 1.4.2 コンボ数のポップアップ表示追加

--- js/sbrsviewer.js
+++ js/sbrsviewer.js
@@ -639,6 +639,7 @@ var SBRSViewer = (function() {
         markerAriaDiv = document.createElement('div');
         markerAriaDiv.className = 'marker-aria';
         markerAriaDiv.style.height = measureHeight + 'px';
+        markerAriaDiv.title = getCombo(markerIndex);
         measureTd.appendChild(markerAriaDiv);
 
         // フィーバ中、ボス攻撃中用のバックグラウンドを描画
@@ -1169,6 +1170,39 @@ var SBRSViewer = (function() {
       lineDiv.style.left = ((viewer.option.laneWidth + 1) * i + -1) + 'px';
       lineAriaDiv.appendChild(lineDiv);
     }
+  }
+
+  /**
+   * コンボ数を取得します
+   * @param {Number} markerIndex マーカーのインデックス
+   * @return {Number} コンボ数
+   */
+  function getCombo(markerIndex) {
+
+    var sbrs = viewer.sbrs;
+    var combo = 0;
+    var marker;
+    var i, iLen;
+
+    for (i = 0, iLen = sbrs.markerCount; i < iLen && i < markerIndex; i++) {
+      marker = sbrs.marker[i];
+      switch (marker.type) {
+        case 1:
+          combo++;
+          break;
+        case 2:
+          combo++;
+          combo += marker.long.length;
+          break;
+        case 3:
+          combo++;
+          break;
+        default:
+          break;
+      }
+    }
+
+    return combo;
   }
 
   /**
